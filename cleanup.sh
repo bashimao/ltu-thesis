6e895984fef949d086659fafebad5b77e94440a9
bashimao <t3l@threelights.de>
2018-10-19 00:34:02 +1100

    Split clean-rebuild.sh script.

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+rm thesis.aux
+rm thesis.bbl
+rm thesis.blg
+rm thesis.loa
+rm thesis.lof
+rm thesis.log
+rm thesis.lol
+rm thesis.lot
+rm thesis.out
+rm thesis.pdf
+rm thesis.run.xml
+rm thesis.synctex.gz
+rm thesis.toc
+rm thesis-blx.bib
